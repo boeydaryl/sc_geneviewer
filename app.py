c4e71e4837188fc599494583fdf41b4a37dd9fb7
Daryl <boeydaryl@gmail.com>
2023-11-13 17:45:20 +0100

    Color scaling now works on both default and manual

--- app.py
+++ app.py
@@ -25,16 +25,17 @@ controls = dbc.Card(
     [
         html.Div(
             [
-                dbc.Label("Latent Representation"),
-                dcc.RadioItems(
-                    id="Lat_rep",
+                dbc.Label("Categorical Data"),
+                dcc.Dropdown(
+                    id="cat_data",
                     options=[
-                        {"label": col, "value": col} for col in obsm_list
+                        {"label": col, "value": col} for col in cat_list
                     ],
-                    value="X_umap",
+                    value="cell_type",
                 ),
             ]
         ),
+        html.Br(),
         html.Div(
             [
                 dbc.Label("Gene"),
@@ -47,27 +48,29 @@ controls = dbc.Card(
                 ),
             ]
         ),
+        html.Br(),
         html.Div(
             [
-                dbc.Label("Categorical Data"),
-                dcc.Dropdown(
-                    id="cat_data",
-                    options=[
-                        {"label": col, "value": col} for col in cat_list
-                    ],
-                    value="cell_type",
-                ),
+                dbc.Label('Gene Value Scaling:'),
+                dcc.RadioItems(
+                    options=[{"label":"Default","value":"Default"},
+                             {"label":"Manual","value":"Manual"}],
+                    id='value_scale',
+                    value="Default",
+)
             ]
         ),
+        html.Br(),
         html.Div(
             [
-                dbc.Label('Gene Value Scale'),
+                dbc.Label('Gene Value Manual'),
                 dcc.RangeSlider(
-                    id='range_slider',
+                    id='value_input',
                     min=0,
                     max=10,
                     step=1,
-                    value=[0,5])
+                    value=[0, 5],
+)
             ]
         )
     ],
@@ -83,12 +86,12 @@ app.layout = dbc.Container(
                  style={
         'textAlign': 'center',
         }),
-        dbc.Row([dbc.Col(dcc.Graph(id="cluster-graph"), md=5), dbc.Col(controls, md=2)],
+        dbc.Row([dbc.Col(dcc.Graph(id="cluster-graph"), md=4), dbc.Col(controls, md=2)],
                 align='center',
-                className="h-85",
+                className="h-95",
                 justify='center',
                 ),
-        html.Div(children='Usage: Select either [Gene] or [Categorical Data], and clear the other field.', style={
+        html.Div(children='Usage: Select either [Categorical Data] or [Gene], and clear the other field.', style={
                 'textAlign': 'center',
                 }),
         html.Br(),
@@ -102,26 +105,31 @@ app.layout = dbc.Container(
 
 @app.callback(
     Output("cluster-graph", "figure"),
-    Input("Lat_rep", "value"),
-    Input("gene_var", "value"),
     Input("cat_data", "value"),
-    Input("range_slider", "value")
+    Input("gene_var", "value"),
+    Input("value_scale", "value"),
+    Input("value_input", "value")
 )
 
-def update_graph(Lat_rep, gene_var, cat_data, range_slider):
-    rep = adata.obsm[Lat_rep]
+def update_graph(cat_data, gene_var, value_scale, value_input):
+    rep = adata.obsm['X_umap']
     x_coords = rep[:, 0]
     y_coords = rep[:, 1]
 
     if gene_var in gene_list:
         vals = df[gene_var]
+        if value_scale == "Default":
+            val_input = [0, np.max(vals)]
+        elif value_scale == "Manual":
+            val_input = value_input
         fig = px.scatter(x=x_coords,
                          y=y_coords,
                          color=vals,
                          color_continuous_scale=['lightgrey', 'tomato', 'firebrick', 'darkred'],
-                         range_color=range_slider,
+                         range_color=val_input,
                          title=gene_var,
                          opacity=0.5)
+
     if cat_data in cat_list:
         if df[cat_data].dtype == 'category':
             color_dict = sc.plotting._tools.scatterplots._get_palette(adata, cat_data)
@@ -140,14 +148,16 @@ def update_graph(Lat_rep, gene_var, cat_data, range_slider):
                              title=cat_data,
                              )
     fig.update_layout(
-        xaxis_title=str(Lat_rep + " 1"),
-        yaxis_title=str(Lat_rep + " 2"),
+        xaxis_title=str("UMAP" + " 1"),
+        yaxis_title=str("UMAP" + " 2"),
         xaxis=dict(visible=False),
         yaxis=dict(visible=False),
+        title_x=0.5,
         font=dict(
             family="Arial, monospace",
             size=14,),
-        plot_bgcolor='rgba(0,0,0,0)'
+        plot_bgcolor='rgba(0,0,0,0)',
+        autosize=True
     )
 
     fig.update_traces(marker=dict(size=5))
